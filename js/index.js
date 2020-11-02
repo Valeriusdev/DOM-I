cf6d6708ad8531c83c447cf21a3c5aec6f09b52d
Valeriusdev <kakao757@gmail.com>
2020-11-02 01:41:40 -0800

    message

--- js/index.js
+++ js/index.js
@@ -40,3 +40,50 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+document.querySelector('nav a:nth-child(1)').textContent = siteContent['nav']['nav-item-1']
+document.querySelector('nav a:nth-child(2)').textContent = siteContent['nav']['nav-item-2']
+document.querySelector('nav a:nth-child(3)').textContent = siteContent['nav']['nav-item-3']
+document.querySelector('nav a:nth-child(4)').textContent = siteContent['nav']['nav-item-4']
+document.querySelector('nav a:nth-child(5)').textContent = siteContent['nav']['nav-item-5']
+document.querySelector('nav a:nth-child(6)').textContent = siteContent['nav']['nav-item-6']
+
+let navItems = document.querySelectorAll('nav a')
+
+navItems.forEach((element) => {
+  return element.style.color = 'green'
+})
+
+document.getElementById('cta-img').setAttribute('src', siteContent['cta']['img-src']);
+document.getElementById('middle-img').setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+document.querySelector('h1').textContent = siteContent['cta']['h1'];
+document.querySelector('button').textContent = siteContent ['cta']['button'];
+document.querySelector('footer p').textContent = siteContent['footer']['copyright'];
+
+document.querySelector('.text-content h4').textContent = siteContent['main-content']['features-h4'];
+document.querySelector('.text-content p').textContent = siteContent['main-content']['features-content'];
+
+document.querySelector('.top-content div:nth-child(2) h4').textContent = siteContent['main-content']['about-h4'];
+document.querySelector('.top-content div:nth-child(2) p').textContent = siteContent['main-content']['about-content'];
+
+document.querySelector('.bottom-content div:nth-child(1) h4').textContent = siteContent['main-content']['services-h4'];
+document.querySelector('.bottom-content div:nth-child(1) p').textContent = siteContent['main-content']['services-content'];
+
+document.querySelector('.bottom-content div:nth-child(2) h4').textContent = siteContent['main-content']['product-h4'];
+document.querySelector('.bottom-content div:nth-child(2) p').textContent = siteContent['main-content']['product-content'];
+
+document.querySelector('.bottom-content div:nth-child(3) h4').textContent = siteContent['main-content']['vision-h4'];
+document.querySelector('.bottom-content div:nth-child(3) p').textContent = siteContent['main-content']['vision-content'];
+
+document.querySelector('.contact h4').textContent = siteContent['contact']['contact-h4'];
+// document.querySelector('.contact p:nth-child(1)').textContent = siteContent['contact']['address'];
+// document.querySelector('.contact p:nth-child(2)').textContent = siteContent['contact']['phone'];
+// document.querySelector('.contact p:nth-child(3)').textContent = siteContent['contact']['email'];
+
+let contactPs = document.querySelectorAll('.contact p')
+contactPs[0].textContent = siteContent ['contact']['address']
+contactPs[1].textContent = siteContent ['contact']['phone']
+contactPs[2].textContent = siteContent ['contact']['email']
+
+
